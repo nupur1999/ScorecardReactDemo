729a4afd8892010b9d4730fd92654b6e43f2f740
Nupur Padmawar <Nupur.Padmawar@lntinfotech.com>
2022-02-14 13:46:27 +0530

    Total row added

--- src/useFetching.js
+++ src/useFetching.js
@@ -11,14 +11,15 @@ const useFetching = (url) => {
           return res.json();
         })
         .then((data) => {
+          let totalPossible = 0,
+            totalScore = 0;
+
           for (let i = 0; i < data.length; i++) {
             let possibleScore = 0,
               score = 0;
             for (let j = 0; j < data[i].children.length; j++) {
               possibleScore += parseFloat(data[i].children[j].Possible);
-              console.log(possibleScore);
               score += parseFloat(data[i].children[j].score);
-              console.log(score);
               data[i].children[j].score_percent =
                 (100 * parseFloat(data[i].children[j].score)) /
                 parseFloat(data[i].children[j].Possible);
@@ -26,10 +27,18 @@ const useFetching = (url) => {
             data[i].Possible = possibleScore;
             data[i].score = score;
             data[i].score_percent = (100 * data[i].score) / data[i].Possible;
+
+            totalPossible += parseFloat(data[i].Possible);
+            totalScore += parseFloat(data[i].score);
           }
+          data.push({
+            Page: "Total",
+            Possible: totalPossible,
+            score: totalScore,
+            score_percent: (100 * totalScore) / totalPossible,
+          });
           setLoaded(true);
           setData(data);
-          console.log(data);
         });
     }, 1000);
   }, [url]);
