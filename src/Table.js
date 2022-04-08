7c818292040a897bf0591b6c5aa8ac580e415d81
npadmawar <nupur.padmawar@lntinfotech.com>
2022-04-08 11:28:40 +0530

    total row change with summary

--- src/Table.js
+++ src/Table.js
@@ -2,15 +2,18 @@ import { Table } from "antd";
 import "antd/dist/antd.css";
 import React, { useState, useEffect } from "react";
 import useFetching from "./useFetching";
+import Typography from '@mui/material/Typography';
 //npx json-server --watch src/db.json --port 8000
 
 const TableData = () => {
-  const { isLoaded ,data: dataSource } = useFetching("http://localhost:8000/dataSource");
+  const { isLoaded ,data: dataSource ,possibleScoreData:possible , scoreData:score , scorePercent:totalPercent} = useFetching("http://localhost:8000/dataSource");
+ // const { Text } = Typography;
   const columns = [
     {
       title: "Page/Table",
       dataIndex: "Page",
       key: "1",
+      //Footer:"Total",
       width: 200,
     },
     {
@@ -43,7 +46,30 @@ const TableData = () => {
   return (
     <div>
        {!isLoaded && <h1>Loading...</h1>}
-     {isLoaded && <Table bordered dataSource={dataSource} columns={columns} />}
+     {isLoaded && <Table 
+     bordered
+      dataSource={dataSource} 
+      columns={columns} 
+      summary={() => {
+        return(
+          <>
+          <Table.Summary.Row>
+           <Table.Summary.Cell>Total</Table.Summary.Cell>
+              <Table.Summary.Cell>
+              {possible}
+              </Table.Summary.Cell>
+              <Table.Summary.Cell>
+               {score}
+              </Table.Summary.Cell> 
+              <Table.Summary.Cell>
+               {totalPercent}
+               </Table.Summary.Cell>  
+           </Table.Summary.Row>
+          </>
+        )
+      }
+      }     
+      />}
     </div>
   );
 };
--- src/useFetching.js
+++ src/useFetching.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 const useFetching = (url) => {
   const [data, setData] = useState(null);
   const [isLoaded, setLoaded] = useState(false);
+  const [possibleScoreData , setPossibleScoreData]=useState(0);
+  const[scoreData , setScoreData]=useState(0);
+  const[scorePercent , setScorePercent]=useState(0);
 
   useEffect(() => {
     setTimeout(() => {
@@ -12,7 +15,8 @@ const useFetching = (url) => {
         })
         .then((data) => {
           let totalPossible = 0,
-            totalScore = 0;
+            totalScore = 0,
+            score_percent=0;
 
           for (let i = 0; i < data.length; i++) {
             let possibleScore = 0,
@@ -30,20 +34,25 @@ const useFetching = (url) => {
 
             totalPossible += parseFloat(data[i].Possible);
             totalScore += parseFloat(data[i].score);
+            
           }
-          data.push({
-            Page: "Total",
-            Possible: totalPossible,
-            score: totalScore,
-            score_percent: (100 * totalScore) / totalPossible,
-          });
+          score_percent = (100*totalScore)/totalPossible;
+          // data.push({
+          //   Page: "Total",
+          //   Possible: totalPossible,
+          //   score: totalScore,
+          //   score_percent: (100 * totalScore) / totalPossible,
+          // });
           setLoaded(true);
           setData(data);
+          setPossibleScoreData(totalPossible);
+          setScoreData(totalScore)
+          setScorePercent(score_percent)
         });
     }, 1000);
   }, [url]);
 
-  return { isLoaded, data };
+  return { isLoaded, data , possibleScoreData ,scoreData , scorePercent};
 };
 
 export default useFetching;
